1a400ee2e95f08685c553eb3d1691166dafd1685
KarimovFarda <68191032+KarimovFarda@users.noreply.github.com>
2021-10-20 17:13:56 +0400

    Delete src/data directory

--- src/data/socialsData.js
+++
@@ -1,9 +0,0 @@
-export const socialsData = {
-    github: 'https://github.com/KarimovFarda',
-    facebook: 'https://www.facebook.com/',
-    linkedIn: 'https://www.linkedin.com/in/farda-karimov-8a00a9183/',
-    codepen: 'https://codepen.io/',
-    medium: 'https://medium.com/@',
-    stackOverflow: 'https://stackoverflow.com/users/',
-
-}

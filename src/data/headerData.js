c0b3ba631123587bbab14524e91eaba58451ebd8
KarimovFarda <68191032+KarimovFarda@users.noreply.github.com>
2021-10-21 14:38:59 +0400

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,10 @@
 import resume from '../assets/pdf/resume.pdf'
+import profilePicture from '../assets/svg/profile/IMG_20210422_205009.jpg'
 
 export const headerData = {
     name: 'KARIMOV FARDA',
     title: "Frontend Web Developer",
     desciption:"When everything seems to be going against you, remember that the airplane takes off against the wind, not with it.",
-    image: 'https://cybertechacademy.az/wp-content/uploads/2021/09/IMG_20210422_205009-6-430x450.jpg',
+    image: profilePicture,
     resumePdf: resume
 }
